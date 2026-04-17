a8134e1ab2379e65f924234af9532c55a569ff0d
Metehan <admin@animus.app>
2026-04-17 22:13:27 +0300

    Yayinla.bat guncellendi: Vercel deploy eklendi

--- Yayinla.bat
+++ Yayinla.bat
@@ -1,6 +1,12 @@
 @echo off
+echo [1/3] Degisiklikler kaydediliyor...
 git add .
 git commit -m "Yeni icerik guncellendi"
 git push
-echo Yayinlandi!
+
+echo [2/3] Vercel'e deploy ediliyor...
+vercel --prod --yes
+
+echo [3/3] Tamamlandi!
+echo Siteniz canli: https://gulceoglu-psikoteknik.vercel.app
 pause
